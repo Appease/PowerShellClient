31bed55a44957ed739a71092b8ae8a7b4dc92285
Chris Dostert <chris.dostert@live.com>
2015-01-29 19:01:09 -0800

    removed unnecessary lines

--- Build/Chocolatey/chocolateyInstall.ps1
+++ Build/Chocolatey/chocolateyInstall.ps1
@@ -2,12 +2,6 @@ try {
     
     . "$PSScriptRoot\Posh-CI\Install.ps1"
 
-    Write-Host $env:PSModulePath
-    Update-SessionEnvironment
-    Write-Host $env:PSModulePath
-
-    Import-Module Posh-CI -Force
-
     Write-ChocolateySuccess 'Posh-CI'
 
 } catch {
